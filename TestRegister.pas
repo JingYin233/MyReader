eb599fefa5b170e8f5e1dfe7c1e9bc69feec7c7e
jinjiayin <1545912679@qq.com>
2026-08-31 17:45:45 +0800

    Initial commit

---
+++ TestRegister.pas
@@ -0,0 +1,86 @@
+unit TestRegister;
+
+interface
+
+procedure Register;
+
+implementation
+
+uses
+  System.Classes,
+  System.SysUtils,
+  Vcl.Menus,
+  ToolsAPI,
+  TestDockForm,
+  DeskForm,
+  DeskUtil;
+
+
+type
+  TTestMenu = class
+  public
+    class procedure OpenTestDock(Sender: TObject);
+  end;
+
+
+var
+  TestDockForm: TTestDockForm;
+
+procedure CreateTestDockForm;
+begin
+  if not Assigned(TestDockForm) then
+  begin
+    TestDockForm := TTestDockForm.Create(nil);
+    RegisterFieldAddress(
+      'TestDockForm',
+      @TestDockForm
+    );
+  end;
+end;
+
+class procedure TTestMenu.OpenTestDock(Sender: TObject);
+begin
+  CreateTestDockForm;
+  ShowDockableForm(TestDockForm);
+end;
+
+
+procedure AddTestMenu;
+var
+  Services: INTAServices;
+  ViewMenu: TMenuItem;
+  Item: TMenuItem;
+begin
+  Services := BorlandIDEServices as INTAServices;
+
+  ViewMenu := Services.MainMenu.Items.Find('View');
+
+  if Assigned(ViewMenu) then
+  begin
+    Item := TMenuItem.Create(ViewMenu);
+
+    Item.Caption := 'Test Dock';
+    Item.OnClick := TTestMenu.OpenTestDock;
+
+    ViewMenu.Add(Item);
+  end;
+end;
+
+
+procedure Register;
+begin
+  CreateTestDockForm;
+
+  AddTestMenu;
+end;
+
+
+initialization
+
+  RegisterDesktopFormClass(
+  TTestDockForm,
+  'TestDockSection',
+  'TestDockForm'
+);
+
+end.
